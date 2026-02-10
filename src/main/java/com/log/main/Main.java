f68d2ff06c8989760afcad07b315458262c20c84
Praneet Reddy <praneetm178@gmail.com>
2026-02-10 14:36:38 +0530

    feat(mainpage):add mainpage

--- src/main/java/com/log/main/Main.java
+++ src/main/java/com/log/main/Main.java
@@ -2,6 +2,7 @@ package com.log.main;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,8 +10,8 @@ import javafx.stage.Stage;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-       class FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/log/ui/MainView.fxml"));
+    public void start(Stage stage)  throws Exception {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/log/ui/MainView.fxml"));
         Scene scene = new Scene(loader.load(), 640, 400);
 
         stage.setTitle("Batchlog");
