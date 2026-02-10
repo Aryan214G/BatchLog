feee9307bc0f7001d5837d4fcc4764d38ee4ab57
Praneet Reddy <praneetm178@gmail.com>
2026-02-10 20:05:25 +0530

    feat(mainpage):add mainpage

--- src/main/java/com/log/main/Main.java
+++ src/main/java/com/log/main/Main.java
@@ -6,8 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main extends Application {
     @Override
     public void start(Stage stage)  throws Exception {
