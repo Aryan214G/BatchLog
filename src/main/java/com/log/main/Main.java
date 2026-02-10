70458d8b51b049bd333f2bc4d5e9e0a644797535
Arn412 <rabhignan@gmail.com>
2026-02-10 16:07:36 +0530

    chore: changed home file

--- src/main/java/com/log/main/Main.java
+++ src/main/java/com/log/main/Main.java
@@ -1,19 +1,26 @@
 package com.log.main;
 
-import com.log.main.UserDAO;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-        UserDAO.createTable();
-        stage.setTitle("SQLite practice");
+    public void start(Stage stage)  throws Exception {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/log/ui/MainView.fxml"));
+        Scene scene = new Scene(loader.load(), 640, 400);
+
+        stage.setTitle("Batchlog");
+        stage.setScene(scene);
         stage.show();
+
     }
     public static void main(String[] args) {
         launch();
     }
 }
+
