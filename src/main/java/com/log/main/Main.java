a9e2f2ce3ee5d3424017675e95c9a1d88bea5f2b
Aryan214G <aryan214gm@gmail.com>
2026-02-10 17:08:53 +0530

    feat(ui): add PropertiesPage

--- src/main/java/com/log/main/Main.java
+++ src/main/java/com/log/main/Main.java
@@ -2,16 +2,23 @@ package com.log.main;
 
 import com.log.main.UserDAO;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-        UserDAO.createTable();
-        stage.setTitle("SQLite practice");
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/log/ui/PropertiesPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
+
     }
     public static void main(String[] args) {
         launch();
