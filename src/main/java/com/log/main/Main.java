ee0511ff90e5a14e60b34f335ff4a8629a5febd1
Arn412 <rabhignan@gmail.com>
2026-02-10 16:25:44 +0530

    feat: change BaseProperties to GridPane Layout from AnchorPane

--- src/main/java/com/log/main/Main.java
+++ src/main/java/com/log/main/Main.java
@@ -11,7 +11,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage)  throws Exception {
-        FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/log/ui/MainView.fxml"));
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/log/ui/BaseProperties.fxml"));
         Scene scene = new Scene(loader.load(), 640, 400);
 
         stage.setTitle("Batchlog");
