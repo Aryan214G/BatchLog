aeda2053ccc849d5264722ec9c44aabc9ee0238f
Praneet Reddy <praneetm178@gmail.com>
2026-02-13 11:41:57 +0530

    fix(test) configure test class for test

--- src/main/java/com/log/main/Test.java
+++ src/main/java/com/log/main/Test.java
@@ -1,4 +1,36 @@
 package com.log.main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Test {
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class Test extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        {
+            FXMLLoader loader = new FXMLLoader(Test.class.getResource("/com/log/ui/views/Mainview.fxml"));
+
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+
+            scene.getStylesheets().add(
+                    Test.class.getResource("/com/log/ui/styles/mainpage.css").toExternalForm()
+            );
+
+            stage.setTitle("BatchLog");
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
