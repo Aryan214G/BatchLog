8b38eea757d531184e4b66f72a155df1ccec1392
Arn412 <rabhignan@gmail.com>
2026-02-17 14:58:25 +0530

    feat(unitsDropdownController): finish controller functionality

--- src/main/java/com/log/ui/UnitsDropdownController.java
+++ src/main/java/com/log/ui/UnitsDropdownController.java
@@ -1,4 +1,37 @@
 package com.log.ui;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class UnitsDropdownController {
+
+    @FXML
+    private ComboBox<String> unitsCombo;
+
+    @FXML
+    public void initialize() {
+
+        unitsCombo.setItems(FXCollections.observableArrayList(
+                "mm", "cm", "m",
+                "kg", "g",
+                "N", "kN",
+                "MPa", "GPa",
+                "°C", "K",
+                "s", "min"
+        ));
+    }
+
+    public String getSelectedUnit() {
+        return unitsCombo.getValue();
+    }
+
+    public void setSelectedUnit(String unit) {
+        unitsCombo.setValue(unit);
+    }
+
+    public ComboBox<String> getComboBox() {
+        return unitsCombo;
+    }
 }
+
