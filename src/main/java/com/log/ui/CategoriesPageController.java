d484dacbcb3697224d1916f6f3c6cd835cf6c4be
Aryan214G <aryan214gm@gmail.com>
2026-03-03 13:34:07 +0530

    refactor(ui): use service class to load temp data

--- src/main/java/com/log/ui/CategoriesPageController.java
+++ src/main/java/com/log/ui/CategoriesPageController.java
@@ -4,6 +4,7 @@ import com.log.core.AppState;
 import com.log.model.PropertyState;
 import com.log.model.Reading;
 import com.log.service.StatisticsService;
+import com.log.service.TempDataService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -67,24 +68,19 @@ public class CategoriesPageController {
     private UnitsDropdownController tempUnitController;
     private DirectionDropdownController directionController;
 
+    private TempDataService tempDataService = new TempDataService();
+
     // ======================= END OF VARIABLES DECLARATION ==============================
 
     @FXML
     public void initialize() throws IOException {
 
         if(!instance.isProjectCreated()) {
-            categoriesListView.setDisable(true);
-            propertiesListView.setDisable(true);
+            categoriesListView.setDisable(false);
+            propertiesListView.setDisable(false);
         }
 
-
-        if (categoriesMap.isEmpty()) {
-            loadTempData();
-        }
-
-        if(defaultRowsMap.isEmpty()) {
-            loadDefaultRowsTempData();
-        }
+        loadTempData();
 
         categoriesListView.setItems(categories);
         loadProperties();
@@ -98,6 +94,14 @@ public class CategoriesPageController {
         deleteItem.setOnAction(e -> handleDeleteCategory());
 
         editMenu = new ContextMenu(addItem, deleteItem);
+    }
+
+    private void loadTempData(){
+        tempDataService.loadTempData();
+        categoriesMap = instance.getCategoriesMap();
+
+        tempDataService.loadDefaultRowsTempData();
+        defaultRowsMap = instance.getDefaultRowsMap();
     }
 
     @FXML
@@ -157,79 +161,6 @@ public class CategoriesPageController {
         categoriesMap.remove(selectedCategory);
     }
 
-    //=============== temporary data ===================
-    private void loadTempData() {
-        categoriesMap.put("Physical",
-                FXCollections.observableArrayList(
-                        "Density",
-                        "Open porosity"
-        ));
-
-        categoriesMap.put("Mechanical",
-                FXCollections.observableArrayList(
-                        "Tensile Strength",
-                        "Tensile Modulus",
-                        "Compressive Strength",
-                        "Compressive Modulus",
-                        "Flexural Strength",
-                        "Flexural Modulus"
-                ));
-
-        categoriesMap.put("Thermal",
-                FXCollections.observableArrayList(
-                        "Specific Heat",
-                        "Thermal Diffusivity",
-                        "Thermal conductivity",
-                        "Mass Loss(%)",
-                        "Coefficient of thermal expansion"
-                ));
-
-        categoriesMap.put("Tribological",
-                FXCollections.observableArrayList(
-                        "Coefficient of friction",
-                        "Wear Rate"
-                ));
-
-        categoriesMap.put("Microstructure",
-                FXCollections.observableArrayList(
-                        "ASTM grain size no.",
-                        "Grain size"
-                ));
-
-        instance.setCategoriesMap(categoriesMap);
-    }
-
-    private void loadDefaultRowsTempData() {
-
-        // Physical
-        defaultRowsMap.put("Density", 6);
-        defaultRowsMap.put("Open porosity", 3);
-
-        // Mechanical
-        defaultRowsMap.put("Tensile Strength", 6);
-        defaultRowsMap.put("Tensile Modulus", 6);
-        defaultRowsMap.put("Compressive Strength", 6);
-        defaultRowsMap.put("Compressive Modulus", 6);
-        defaultRowsMap.put("Flexural Strength", 6);
-        defaultRowsMap.put("Flexural Modulus", 6);
-
-        // Thermal
-        defaultRowsMap.put("Specific Heat", 3);
-        defaultRowsMap.put("Thermal Diffusivity", 3);
-        defaultRowsMap.put("Thermal conductivity", 3);
-        defaultRowsMap.put("Mass Loss(%)", 5);
-        defaultRowsMap.put("Coefficient of thermal expansion", 3);
-
-        // Tribological
-        defaultRowsMap.put("Coefficient of friction", 5);
-        defaultRowsMap.put("Wear Rate", 5);
-
-        // Microstructure
-        defaultRowsMap.put("ASTM grain size no.", 3);
-        defaultRowsMap.put("Grain size", 3);
-
-        instance.setDefaultRowsMap(defaultRowsMap);
-    }
 
 
     //TODO: loadProperties is doing multiple tasks. Either change the name of the method or divide the responsibilities
