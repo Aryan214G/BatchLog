0d25eb0291d561d6f52ac3dabd506a7f6349a7c2
Aryan214G <aryan214gm@gmail.com>
2026-04-03 14:40:39 +0530

    test(PropertyService): add initial test class for PropertyService

---
+++ src/test/java/PropertyServiceTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyServiceTest {
+
+    @Test
+    void insertProperty() {
+    }
+
+    @Test
+    void insertPropertyValues() {
+    }
+}
