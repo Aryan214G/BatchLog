efbd85e3dadd061ebfa72e9e94aa6a1c34463261
praneet1508 <praneetm178@gmail.com>
2026-05-18 11:33:05 +0530

    feat(cat):add search

---
+++ src/main/java/com/log/ui/SearchBarController.java
@@ -0,0 +1,14 @@
+package com.log.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SearchBarController {
+
+    @FXML
+    private TextField searchField;
+
+    public TextField getSearchField() {
+        return searchField;
+    }
+}
