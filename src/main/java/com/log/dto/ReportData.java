48edfede32685404be2cbb74ddcdb62d69c69348
Aryan214G <aryan214gm@gmail.com>
2026-05-31 16:18:47 +0530

    feat(report): add propertyId and statistical fields to report data model

--- src/main/java/com/log/dto/ReportData.java
+++ src/main/java/com/log/dto/ReportData.java
@@ -12,4 +12,66 @@ public class ReportData {
     private Batch batch;
     private List<PropertyRow> properties;
 
+    private Double min;
+    private Double max;
+    private Double average;
+    private Double standardDeviation;
+
+    private int testReportNo;
+
+    public BatchTest getBatchTest() {
+        return batchTest;
+    }
+
+    public void setBatchTest(BatchTest batchTest) {
+        this.batchTest = batchTest;
+    }
+
+    public Batch getBatch() {
+        return batch;
+    }
+
+    public void setBatch(Batch batch) {
+        this.batch = batch;
+    }
+
+    public List<PropertyRow> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<PropertyRow> properties) {
+        this.properties = properties;
+    }
+
+    public Double getMin() {
+        return min;
+    }
+
+    public void setMin(Double min) {
+        this.min = min;
+    }
+
+    public Double getMax() {
+        return max;
+    }
+
+    public void setMax(Double max) {
+        this.max = max;
+    }
+
+    public Double getAverage() {
+        return average;
+    }
+
+    public void setAverage(Double average) {
+        this.average = average;
+    }
+
+    public Double getStandardDeviation() {
+        return standardDeviation;
+    }
+
+    public void setStandardDeviation(Double standardDeviation) {
+        this.standardDeviation = standardDeviation;
+    }
 }
