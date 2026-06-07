3a1270282041c8a4190b8b904720485bc7a48817
Aryan214G <aryan214gm@gmail.com>
2026-06-07 14:35:05 +0530

    feat(pdf): implement layout generation for first section of the report

--- src/main/java/com/log/service/export/RetrievalTablePdfService.java
+++ src/main/java/com/log/service/export/RetrievalTablePdfService.java
@@ -2,11 +2,13 @@ package com.log.service.export;
 
 import com.log.dto.RetrievalTableReportData;
 import com.log.util.pdf.PdfConstants;
+import com.log.util.pdf.PdfLayoutUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 
 import static com.log.util.pdf.PdfUtils.writeBoldText;
+import static com.log.util.pdf.PdfUtils.writeText;
 
 public class RetrievalTablePdfService
         implements Exporter<RetrievalTableReportData> {
@@ -23,14 +25,63 @@ public class RetrievalTablePdfService
             try (PDPageContentStream content =
                          new PDPageContentStream(document, page)) {
 
+                 // ========= CONSTANTS =============================================
                 float y = PdfConstants.TOP_Y;
+                float leftMargin = PdfLayoutUtils.getLeftMargin(page);
+                float center = PdfLayoutUtils.getPageCenterX(page);
 
-                writeBoldText(
+                 // ==================================================================
+
+                writeBoldText(content, "REPORT", center, y);
+
+                // ========= TOP SECTION =============================================
+
+                y -= PdfConstants.SECTION_SPACING;
+
+                writeText(
                         content,
-                        "REPORT",
-                        250,
+                        "Project No. :",
+                        leftMargin,
                         y
                 );
+
+                y -= PdfConstants.LINE_SPACING;
+
+                writeText(
+                        content,
+                        "SOP :",
+                        leftMargin,
+                        y
+                );
+
+                y -= PdfConstants.LINE_SPACING;
+
+                writeText(
+                        content,
+                        "Product :",
+                        leftMargin,
+                        y
+                );
+
+                y -= PdfConstants.LINE_SPACING;
+
+                writeText(
+                        content,
+                        "Component ID / Batch No. :",
+                        leftMargin,
+                        y
+                );
+
+                y -= PdfConstants.LINE_SPACING;
+
+                writeText(
+                        content,
+                        "Test Schedule :",
+                        leftMargin,
+                        y
+                );
+
+                y -= PdfConstants.SECTION_SPACING;
             }
             document.save("retrieval-report.pdf");
         }
