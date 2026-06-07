3378237abece63402b78f70195ffa6296ff96c91
Arn412 <rabhignan@gmail.com>
2026-06-07 15:51:11 +0530

    feat(infoBarController): add remaining baseProperties

--- src/main/java/com/log/ui/InfoBarController.java
+++ src/main/java/com/log/ui/InfoBarController.java
@@ -8,8 +8,11 @@ import javafx.scene.control.Button;
 
 public class InfoBarController {
 
+    @FXML private Button infobar_projectName;
     @FXML private Button infobar_productName;
     @FXML private Button infobar_batchNo;
+    @FXML private Button infobar_testDate;
+    @FXML private Button infobar_testSite;
     @FXML private Button infobar_propertyName;
     @FXML private Button infobar_attributeName;
 
@@ -24,11 +27,22 @@ public class InfoBarController {
 
     public void refresh() {
 
+        infobar_projectName.setText(
+                "Project: " + safe(bpropState.getProjectName()));
+
         infobar_productName.setText(
                 "Product: " + safe(bpropState.getProductName()));
 
         infobar_batchNo.setText(
                 "Batch: " + safe(bpropState.getBatchNo()));
+
+        infobar_testDate.setText(
+                "Date: " + (bpropState.getTestDate() != null
+                        ? bpropState.getTestDate().toString()
+                        : "-"));
+
+        infobar_testSite.setText(
+                "Site: " + safe(bpropState.getPlaceOfTesting()));
 
         infobar_propertyName.setText(
                 safe(selectedState.getSelectedCategory()));
