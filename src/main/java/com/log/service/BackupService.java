5e856f8b0a10c99e65769d01f444c60bd0a95939
Aryan214G <aryan214gm@gmail.com>
2026-06-10 15:36:40 +0530

    feat(backup): implement BackupService with methods for creating and restoring backups

---
+++ src/main/java/com/log/service/BackupService.java
@@ -0,0 +1,19 @@
+package com.log.service;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class BackupService {
+
+    public void createBackup(Path destination)
+            throws IOException {
+    }
+
+    public void restoreBackup(Path backupFile)
+            throws IOException {
+    }
+
+    private Path createSafetyBackup()
+            throws IOException {
+    }
+}
