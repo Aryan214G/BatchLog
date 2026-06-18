8e047bb883bf8071746b26de1781ccf8b8a4f5d0
Aryan214G <aryan214gm@gmail.com>
2026-06-18 23:32:59 +0530

    fix(DBUtil): disable testing mode

--- src/main/java/com/log/database/DBUtil.java
+++ src/main/java/com/log/database/DBUtil.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 
 public class DBUtil {
 
-    private static final boolean TESTING = true;
+    private static final boolean TESTING = false;
 
     private static final Path APP_DIR =
         Paths.get(
