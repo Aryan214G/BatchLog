5c7ccdf7fd7c9fb1fec505b457e6cc49fdde6ca9
Arn412 <rabhignan@gmail.com>
2026-06-20 19:31:46 +0530

    fix(DBUtil): make dbutil cross platform

--- src/main/java/com/log/database/DBUtil.java
+++ src/main/java/com/log/database/DBUtil.java
@@ -16,19 +16,74 @@ public class DBUtil {
 
     private static final boolean TESTING = true;
 
-    private static final Path APP_DIR =
-        Paths.get(
-                System.getenv("LOCALAPPDATA"),
-                "BatchLog"
-        );
+    private static final Path APP_DIR;
+    private static final Path DB_PATH;
+    private static final String URL;
 
-    private static final Path DB_PATH = (TESTING)
-        ? Paths.get("database", "BatchLog_test.db")
-        : APP_DIR.resolve(
-                Paths.get("database", "BatchLog.db")
-    );
+    static {
 
-    private static final String URL = "jdbc:sqlite:" + DB_PATH.toAbsolutePath();
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("win")) {
+
+            APP_DIR = Paths.get(
+                    System.getenv("LOCALAPPDATA"),
+                    "BatchLog"
+            );
+
+        } else if (os.contains("mac")) {
+
+            APP_DIR = Paths.get(
+                    System.getProperty("user.home"),
+                    "Library",
+                    "Application Support",
+                    "BatchLog"
+            );
+
+        } else {
+
+            APP_DIR = Paths.get(
+                    System.getProperty("user.home"),
+                    ".batchlog"
+            );
+        }
+
+        DB_PATH = TESTING
+                ? Paths.get("database", "BatchLog_test.db")
+                : APP_DIR.resolve("database")
+                .resolve("BatchLog.db");
+
+        URL = "jdbc:sqlite:" + DB_PATH.toAbsolutePath();
+
+        try {
+
+            if (!TESTING) {
+
+                Files.createDirectories(
+                        APP_DIR.resolve("database")
+                );
+
+                if (!Files.exists(DB_PATH)) {
+                    copyMasterDatabase();
+                }
+            }
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(
+                    "Failed to initialize database",
+                    e
+            );
+        }
+    }
+
+//    private static final Path DB_PATH = (TESTING)
+//        ? Paths.get("database", "BatchLog_test.db")
+//        : APP_DIR.resolve(
+//                Paths.get("database", "BatchLog.db")
+//    );
+//
+//    private static final String URL = "jdbc:sqlite:" + DB_PATH.toAbsolutePath();
 
 
     static {
