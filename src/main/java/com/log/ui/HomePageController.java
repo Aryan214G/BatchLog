0cef3d07b5f9d75260027c7abaae08ac9af8f989
Arn412 <rabhignan@gmail.com>
2026-06-21 12:46:56 +0530

    chore(HomePageController): remove delete project option

--- src/main/java/com/log/ui/HomePageController.java
+++ src/main/java/com/log/ui/HomePageController.java
@@ -178,13 +178,13 @@ public class HomePageController implements Initializable {
 
         MenuItem openItem   = new MenuItem("Open");
         MenuItem renameItem = new MenuItem("Rename");
-        MenuItem deleteItem = new MenuItem("Delete");
+//        MenuItem deleteItem = new MenuItem("Delete");
 
         openItem.setOnAction(e -> handleProjectCardOpen(projectName));
         renameItem.setOnAction(e -> handleProjectCardEdit(projectName));
-        deleteItem.setOnAction(e -> handleProjectCardDelete(projectName));
+//        deleteItem.setOnAction(e -> handleProjectCardDelete(projectName));
 
-        contextMenu.getItems().addAll(openItem, renameItem, deleteItem);
+        contextMenu.getItems().addAll(openItem, renameItem);
         return contextMenu;
     }
 
