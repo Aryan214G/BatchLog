5b85967275648685aee71f073d8d60c3f5916d54
Aryan214G <aryan214gm@gmail.com>
2026-06-21 15:29:37 +0530

    feat(UI): add global styles for colors, typography, and spacing

--- src/main/java/com/log/main/Main.java
+++ src/main/java/com/log/main/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import com.log.database.DBUtil;
 import javafx.util.Duration;
@@ -20,9 +21,9 @@ public class Main extends Application {
 
     public Main() throws SQLException {
 
-            System.out.println(DBUtil.getDatabasePath().toAbsolutePath());
+        System.out.println(DBUtil.getDatabasePath().toAbsolutePath());
 
-        try(Connection conn = DBUtil.getConnection()){
+        try (Connection conn = DBUtil.getConnection()) {
 
             if (conn != null) {
                 System.out.println("Connected to SQLite database!");
@@ -33,63 +34,90 @@ public class Main extends Application {
         }
     }
 
-    @Override
-public void start(Stage stage) throws IOException {
-
-    FXMLLoader loader = new FXMLLoader(
-            getClass().getResource("/com/log/ui/views/HomePage.fxml")
-    );
-
-    Parent root = loader.load();
-
-    Scene scene = new Scene(root);
-
-    scene.getStylesheets().add(
-            getClass()
-                    .getResource("/com/log/ui/styles/pages/homePage.css")
-                    .toExternalForm()
-    );
-
-    // =========================================
-    // Splash Screen
-    // =========================================
-
-    SplashScreen splash = new SplashScreen();
-
-    splash.show();
-
-    PauseTransition delay = new PauseTransition(Duration.seconds(2));
-
-    delay.setOnFinished(e -> {
-
-        splash.close();
-
-        stage.setTitle("BatchLog");
-        stage.setScene(scene);
-        stage.show();
-    });
-
-    delay.play();
-}
-
-    private void handleJavaFXLogging(){
-        Logger rootLogger = Logger.getLogger("");
-
-        for (Handler handler : rootLogger.getHandlers()) {
-            handler.setFilter(record -> {
-                String msg = record.getMessage();
-                if (msg != null && msg.contains("Loading FXML document with JavaFX API of version")) {
-                    return false;
-                }
-                return true;
-            });
-        }
-    }
     public static void main(String[] args) throws SQLException {
         Main obj = new Main();
         obj.handleJavaFXLogging();
 
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        loadFonts();
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/com/log/ui/views/HomePage.fxml")
+        );
+
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        scene.getStylesheets().add(
+                getClass()
+                        .getResource("/com/log/ui/styles/pages/homePage.css")
+                        .toExternalForm()
+        );
+
+        // =========================================
+        // Splash Screen
+        // =========================================
+
+        SplashScreen splash = new SplashScreen();
+
+        splash.show();
+
+        PauseTransition delay = new PauseTransition(Duration.seconds(2));
+
+        delay.setOnFinished(e -> {
+
+            splash.close();
+
+            stage.setTitle("BatchLog");
+            stage.setScene(scene);
+            stage.show();
+        });
+
+        delay.play();
+    }
+
+    private void handleJavaFXLogging() {
+        Logger rootLogger = Logger.getLogger("");
+
+        for (Handler handler : rootLogger.getHandlers()) {
+            handler.setFilter(record -> {
+                String msg = record.getMessage();
+                return msg == null || !msg.contains("Loading FXML document with JavaFX API of version");
+            });
+        }
+    }
+
+    public void loadFonts() {
+        Font.loadFont(
+                getClass().getResourceAsStream(
+                        "/com/log/ui/fonts/Inter_18pt-Regular.ttf"),
+                14
+        );
+
+        Font.loadFont(
+                getClass().getResourceAsStream(
+                        "/com/log/ui/fonts/Inter_18pt-SemiBold.ttf"),
+                14
+        );
+
+        Font.loadFont(
+                getClass().getResourceAsStream(
+                        "/com/log/ui/fonts/Inter_18pt-Bold.ttf"),
+                14
+        );
+
+        System.out.println(
+                Font.loadFont(
+                        getClass().getResourceAsStream(
+                                "/com/log/ui/fonts/Inter_18pt-Regular.ttf"),
+                        14
+                ).getFamily()
+        );
     }
 }
 
